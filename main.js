eef50b601efdd0b3124a82ac296061e2dddde2ee
Jonathan Aspeling <jbaspeling@gmail.com>
2022-06-12 14:45:46 +0200

    Full Lesson Commit - Including Test Answers For Part 7

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const app = Vue.createApp({
             cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ],
         }
     },
@@ -29,6 +28,14 @@ const app = Vue.createApp({
         },
         removeFromCart() {
             this.cart -= 1;
+        },
+        updateVariant(){
+
         }
     },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        }
+    }
 })
